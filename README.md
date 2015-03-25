1a79b6e23c84a736d573ce93ccc9336fe8fa94d9
Johannes Lange <johannes.lange@rwth-aachen.de>
2015-03-25 14:02:16 +0000

    some readme markup

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 *TreeWriter* to build a tree from MiniAOD. Photon Cut-IDs and MVA are computed.
 
 ### Photons ### 
-- based on this recipe for MVA ID: https://hypernews.cern.ch/HyperNews/CMS/get/egamma/1552.html
-  * "git cms-merge-topic …" necessary!
-- this Cut-ID is included manually: https://hypernews.cern.ch/HyperNews/CMS/get/egamma/1541.html
+- based on this recipe for MVA ID: [HN](https://hypernews.cern.ch/HyperNews/CMS/get/egamma/1552.html)
+  * `git cms-merge-topic …` necessary!
+- this Cut-ID is included manually: [HN](https://hypernews.cern.ch/HyperNews/CMS/get/egamma/1541.html)
 
 
 ### Jets ###
@@ -17,8 +17,9 @@
 ### Electrons ###
 - fulfilling "veto" id
 - boolean flags for loose/medium/tight
-- recipes: https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedElectronIdentificationRun2#Recipe_for_regular_users_for_min
+- recipes: [TWiki](https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedElectronIdentificationRun2#Recipe_for_regular_users_for_min)
 
-- generate dictionaries (does not seem necessary, cmssw does so already)
-$ rootcint -f ROOT_dicts.cpp -c -I. TreeParticles.hpp LinkDef.h 
+- generate dictionaries (not necessary, cmssw does so from LinkDef.h already)
+
+    $ rootcint -f ROOT_dicts.cpp -c -I. TreeParticles.hpp LinkDef.h 
 
--- TreeWriter/python/runTreeWriter.py
+++ TreeWriter/python/runTreeWriter.py
@@ -23,15 +23,15 @@ process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(
    # Just a handful of files from the dataset are listed below, for testing
    #
        # '/store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/101611CC-026E-E411-B8D7-00266CFFBF88.root',
-       '/store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/1024D6DB-7D6F-E411-AE1D-00266CFF0608.root',
+       # '/store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/1024D6DB-7D6F-E411-AE1D-00266CFF0608.root',
        # '/store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/107B7861-7C6F-E411-974E-00266CFFC80C.root',
        # '/store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/12FB6345-C96C-E411-85C9-00266CFFC4D4.root',
         # muons
         # "/store/relval/CMSSW_7_2_0/SingleMu/MINIAOD/PRE_R_72_V10A_RelVal_mu2012D-v2/00000/12AF52D0-945A-E411-A94D-0025905A48C0.root",
         # electrons
-        # '/store/relval/CMSSW_7_2_0/SingleElectron/MINIAOD/PRE_R_72_V10A_RelVal_electron2012D-v2/00000/10721F98-F459-E411-B363-0025905B8610.root'
+        '/store/relval/CMSSW_7_2_0/SingleElectron/MINIAOD/PRE_R_72_V10A_RelVal_electron2012D-v2/00000/10721F98-F459-E411-B363-0025905B8610.root'
    # Run from a local file
-#      'file:/afs/cern.ch/user/i/ikrav/workspace/releases-git/CMSSW_7_2_0/src/ElectronWork/ElectronNtupler/test/00A074A5-BF72-E411-B455-003048F02CBE_mini.root'
+     # 'file:/afs/cern.ch/user/i/ikrav/workspace/releases-git/CMSSW_7_2_0/src/ElectronWork/ElectronNtupler/test/00A074A5-BF72-E411-B455-003048F02CBE_mini.root'
  )
 )
 
