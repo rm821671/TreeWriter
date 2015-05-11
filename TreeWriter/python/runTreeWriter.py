0935f5b1783071994dd48fb4f71620b0bc9a494b
Johannes Lange <johannes.lange@rwth-aachen.de>
2015-05-11 09:11:14 +0200

    +DY input for electron testing later on

--- TreeWriter/python/runTreeWriter.py
+++ TreeWriter/python/runTreeWriter.py
@@ -20,18 +20,19 @@ process.GlobalTag.globaltag = 'PHYS14_25_V1::All'
 #
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(200) )
 process.source = cms.Source ("PoolSource",fileNames = cms.untracked.vstring(
-        # Just a handful of files from the dataset are listed below, for testing
-        'root://xrootd-cms.infn.it///store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/101611CC-026E-E411-B8D7-00266CFFBF88.root',
-        # 'root://xrootd-cms.infn.it///store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/1024D6DB-7D6F-E411-AE1D-00266CFF0608.root',
-        # 'root://xrootd-cms.infn.it///store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/107B7861-7C6F-E411-974E-00266CFFC80C.root',
-        # 'root://xrootd-cms.infn.it///store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/12FB6345-C96C-E411-85C9-00266CFFC4D4.root',
-        # muons
-        # 'root://xrootd-cms.infn.it///store/relval/CMSSW_7_2_0/SingleMu/MINIAOD/PRE_R_72_V10A_RelVal_mu2012D-v2/00000/12AF52D0-945A-E411-A94D-0025905A48C0.root',
-        # electrons
-        # 'root://xrootd-cms.infn.it///store/relval/CMSSW_7_2_0/SingleElectron/MINIAOD/PRE_R_72_V10A_RelVal_electron2012D-v2/00000/10721F98-F459-E411-B363-0025905B8610.root'
-        # Run from a local file
-        # 'file:/afs/cern.ch/user/j/jolange/private/data/minoAOD/singleElectron/964E27EF-9B5A-E411-8E30-0026189438FD.root'
-        ))
+    # Just a handful of files from the dataset are listed below, for testing
+    'root://xrootd-cms.infn.it///store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/101611CC-026E-E411-B8D7-00266CFFBF88.root',
+    'root://xrootd-cms.infn.it///store/mc/Phys14DR/DYJetsToLL_M-50_13TeV-madgraph-pythia8/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/0432E62A-7A6C-E411-87BB-002590DB92A8.root'
+    # 'root://xrootd-cms.infn.it///store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/1024D6DB-7D6F-E411-AE1D-00266CFF0608.root',
+    # 'root://xrootd-cms.infn.it///store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/107B7861-7C6F-E411-974E-00266CFFC80C.root',
+    # 'root://xrootd-cms.infn.it///store/mc/Phys14DR/GJet_Pt40_doubleEMEnriched_TuneZ2star_13TeV-pythia6/MINIAODSIM/PU20bx25_PHYS14_25_V1-v1/00000/12FB6345-C96C-E411-85C9-00266CFFC4D4.root',
+    # muons
+    # 'root://xrootd-cms.infn.it///store/relval/CMSSW_7_2_0/SingleMu/MINIAOD/PRE_R_72_V10A_RelVal_mu2012D-v2/00000/12AF52D0-945A-E411-A94D-0025905A48C0.root',
+    # electrons
+    # 'root://xrootd-cms.infn.it///store/relval/CMSSW_7_2_0/SingleElectron/MINIAOD/PRE_R_72_V10A_RelVal_electron2012D-v2/00000/10721F98-F459-E411-B363-0025905B8610.root'
+    # Run from a local file
+    # 'file:/afs/cern.ch/user/j/jolange/private/data/minoAOD/singleElectron/964E27EF-9B5A-E411-8E30-0026189438FD.root'
+))
 
 
 process.TreeWriter = cms.EDAnalyzer('TreeWriter',
