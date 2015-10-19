b2706f8f1a67b78557307f365abaa616da30baad
Knut Kiesel <kiesel@cern.ch>
2015-10-19 12:14:46 +0200

    jet pt cut to reduce nTuple size

--- TreeWriter/plugins/TreeWriter.cpp
+++ TreeWriter/plugins/TreeWriter.cpp
@@ -419,6 +419,7 @@ TreeWriter::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
    vJets_.clear();
    tree::Jet trJet;
    for (const pat::Jet& jet : *jetColl){
+      if (jet.pt()<30) continue;
       trJet.p.SetPtEtaPhi(jet.pt(),jet.eta(),jet.phi());
       trJet.bDiscriminator=jet.bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags");
       trJet.isLoose=isLooseJet(jet);
