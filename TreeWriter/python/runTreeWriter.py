5a6715d1adf78d6720079a4fd7f4015366061d92
Johannes Lange <johannes.lange@rwth-aachen.de>
2015-11-03 13:33:37 +0100

    GT for prompt reco v4 is recognized

--- TreeWriter/python/runTreeWriter.py
+++ TreeWriter/python/runTreeWriter.py
@@ -38,6 +38,7 @@ gtName = "auto:run2_data" if isRealData else "auto:run2_mc"
 # for further global tags, see here:
 # https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookMiniAOD
 if re.match( "/.*/Run2015.*05Oct2015.*/MINIAOD", options.dataset ): gtName = "74X_dataRun2_reMiniAOD_v0,frontier://FrontierArc/CMS_CONDITIONS_EJ06"
+if re.match( "/.*/Run2015D-PromptReco-v4/MINIAOD", options.dataset ): gtName = "74X_dataRun2_Prompt_v4,frontier://FrontierArc/CMS_CONDITIONS_EJ06"
 if re.match( "/.*/.*RunIISpring15MiniAODv2.*/MINIAODSIM", options.dataset ): gtName = "74X_mcRun2_asymptotic_v2,frontier://FrontierArc/CMS_CONDITIONS_EJ06"
 process.GlobalTag = GlobalTag(process.GlobalTag, gtName, '')
 
